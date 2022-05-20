2ea4d48349ff170b5f818b1cd621a8d157194c4f
Abhisek Padhi <avicool000@gmail.com>
2022-05-20 19:10:20 +0530

    updated ecommerce app

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,71 @@
 import {useEffect, useState} from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { set } from "cookie-toss";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [phone, setPhone] = useState('');
+  const [added, setAdded] = useState(false);
   useEffect(() => {
-    set({
-      // This is the URL at which you've hosted the output of `createIframe`, above:
-      iframeUrl: 'https://tosshub.netlify.app/',
-      dataKey: 'chocolate-chip-oatmeal',
-      data: {
-        c: 'is for localStorage',
-        and: 'that\'s good enough for me.'
-      }
-    }).then(res => {
-      console.log(`set res: ${JSON.stringify(res)}`);
-    }).catch(err => {
-      console.log(`set err: ${JSON.stringify(err)}`);
-    });
+    // save userId post login, in central hub
+
   }, [])
+  const handleAddToCart = () => {
+    let phone = prompt("Login using phone to continue...", '');
+    if (phone !== null) {
+      set({
+        // This is the URL at which you've hosted the output of `createIframe`, above:
+        iframeUrl: 'https://rapydgenie.netlify.app/',
+        dataKey: 'rapidGenieUser',
+        data: {
+          userId: phone,
+        }
+      }).then(res => {
+        console.log(`set res: ${JSON.stringify(res)}`);
+      }).catch(err => {
+        console.log(`set err: ${JSON.stringify(err)}`);
+      });
+      setPhone(phone);
+      setAdded(true)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+      <div className="App">
+        {phone.length > 0 && (<h2>
+          Hi {phone.slice(0, -7) + '*******'}
+        </h2>)}
+        <div>
+          <div>
+            <img src={'https://m.media-amazon.com/images/I/51yFtsNy+qL._SY300_SX300_.jpg'} width={320} height={320} alt={'product'} />
+          </div>
+          <div>
+            <h3>Basmati rice (1KG)</h3>
+            <p>Price: ₹5</p>
+            {added ? (
+                <p>
+                  ✓ Added to cart
+                </p>
+            ) : (<button onClick={handleAddToCart} type="button">
+              <img src="https://icongr.am/clarity/add.svg?size=16&amp;color=000" alt="icon"/>
+              {' '}Add to cart
+            </button>)}
+          </div>
+        </div>
+        {added && (<div style={{
+          position: 'absolute',
+          bottom: 10,
+          left: '45%',
+        }}>
+          <div>
+            1 item
+            ₹5
+          </div>
+          <div>
+            <button type="button">
+              Checkout {' '} →
+            </button>
+          </div>
+        </div>)}
+      </div>
   )
 }
 
